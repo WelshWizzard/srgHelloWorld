81753bd42af1437ab2a4ecc21ead08c09155ce69
Shaw Goodwin <shaw.goodwin@gmail.com>
2019-07-08 07:42:04 +0100

    Commit

---
+++ srgHelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  srgHelloWorld
+//
+//  Created by administrator on 08/07/2019.
+//  Copyright © 2019 Shaw Goodwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
